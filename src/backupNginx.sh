16f80c4375f0b2e7eae8da68dbac47d0e1f12fca
Eero Mikkonen <eero.mikkonen@hotmail.com>
2018-01-28 23:38:47 +0000

    Remove folder after creating tarball

--- src/backupNginx.sh
+++ src/backupNginx.sh
@@ -4,5 +4,6 @@ user=$(whoami)
 mkdir -p /home/$user/backups/nginx/$date
 sudo cp -bR /etc/nginx /home/$user/backups/nginx/$date/
 sudo tar cf /home/$user/backups/nginx/$date/nginx.bak.tar /home/$user/backups/nginx/$date/nginx
+sudo rm -rf /home/$user/backups/nginx/$date/nginx/
 sudo chown $user:$user /home/$user/backups/nginx/$date/nginx.bak.tar
 echo "Backup tarball can be found at /home/$user/backups/nginx/$date/nginx.bak.tar"
--- src/backupletsencrypt.sh
+++ src/backupletsencrypt.sh
@@ -4,5 +4,6 @@ user=$(whoami)
 mkdir -p /home/$user/backups/letsencrypt/$date
 sudo cp -bR /etc/letsencrypt/ /home/$user/backups/letsencrypt/$date
 sudo tar cf /home/$user/backups/letsencrypt/$date/letsencrypt.bak.tar /home/$user/backups/letsencrypt/$date/letsencrypt
+sudo rm -rf /home/$user/backups/letsencrypt/$date/letsencrypt/
 sudo chown $user:$user /home/$user/backups/letsencrypt/$date/letsencrypt.bak.tar
 echo "Backup tarball can be found at /home/$user/backups/letsencrypt/$date/letsencrypt.bak.tar"
