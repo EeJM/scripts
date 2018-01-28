50932cd0fbf80fb0c92bfb7ab3050d2de9fe7176
Eero Mikkonen <eero.mikkonen@hotmail.com>
2018-01-28 17:53:59 +0000

    Make user a variable instead of a fixed name

--- src/backupNginx.sh
+++ src/backupNginx.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 date=$(date +%d-%m-%Y)
-mkdir -p /home/eero/backups/nginx/$date
-sudo cp -bR /etc/nginx /home/eero/backups/nginx/$date/
-sudo tar cf /home/eero/backups/nginx/$date/nginx.bak.tar /home/eero/backups/nginx/$date/nginx
-sudo chown eero:eero /home/eero/backups/nginx/$date/nginx.bak.tar
-echo "Backup tarball can be found at /home/eero/backups/nginx/$date/nginx.bak.tar"
+user=$(whoami)
+mkdir -p /home/$user/backups/nginx/$date
+sudo cp -bR /etc/nginx /home/$user/backups/nginx/$date/
+sudo tar cf /home/$user/backups/nginx/$date/nginx.bak.tar /home/$user/backups/nginx/$date/nginx
+sudo chown $user:$user /home/$user/backups/nginx/$date/nginx.bak.tar
+echo "Backup tarball can be found at /home/$user/backups/nginx/$date/nginx.bak.tar"
--- src/backupletsencrypt.sh
+++ src/backupletsencrypt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 date=$(date +%d-%m-%Y)
-mkdir -p /home/eero/backups/letsencrypt/$date
-sudo cp -bR /etc/letsencrypt/ /home/eero/backups/letsencrypt/$date
-sudo tar cf /home/eero/backups/letsencrypt/$date/letsencrypt.bak.tar /home/eero/backups/letsencrypt/$date/letsencrypt
-sudo chown eero:eero /home/eero/backups/letsencrypt/$date/letsencrypt.bak.tar
-echo "Backup tarball can be found at /home/eero/backups/letsencrypt/$date/letsencrypt.bak.tar"
+user=$(whoami)
+mkdir -p /home/$user/backups/letsencrypt/$date
+sudo cp -bR /etc/letsencrypt/ /home/$user/backups/letsencrypt/$date
+sudo tar cf /home/$user/backups/letsencrypt/$date/letsencrypt.bak.tar /home/$user/backups/letsencrypt/$date/letsencrypt
+sudo chown $user:$user /home/$user/backups/letsencrypt/$date/letsencrypt.bak.tar
+echo "Backup tarball can be found at /home/$user/backups/letsencrypt/$date/letsencrypt.bak.tar"
