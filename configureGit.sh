6801abe04ace8318cbf9d761bad2caa79ab1bae7
Eero Mikkonen <eero.mikkonen@hotmail.com>
2018-01-28 16:43:30 +0000

    Create a script for basic git configuration

---
+++ configureGit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Configuring git"
+git config --global user.name "Eero Mikkonen"
+git config --global user.email eero.mikkonen@hotmail.com
+git config --global core.editor vim
+git config --global credential.helper cache
+echo "Your current git configuration is:"
+git config --global --list
