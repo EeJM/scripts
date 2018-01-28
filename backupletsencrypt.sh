4feb21528e39355ee2653e3952522eaf919f9f5a
Eero Mikkonen <eero.mikkonen@hotmail.com>
2018-01-28 17:02:17 +0000

    Remove scripts from old location

--- backupletsencrypt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-date=$(date +%d-%m-%Y)
-mkdir /home/eero/backups/letsencrypt/$date
-sudo cp -bR /etc/letsencrypt/ /home/eero/backups/letsencrypt/$date
-sudo tar cf /home/eero/backups/letsencrypt/$date/letsencrypt.bak.tar /home/eero/backups/letsencrypt/$date/letsencrypt
-sudo chown eero:eero /home/eero/backups/letsencrypt/$date/letsencrypt.bak.tar
-echo "Backup tarball can be found at /home/eero/backups/letsencrypt/$date/letsencrypt.bak.tar"
